982de1237736a0db4e395690aea8461b79b921c6
Pablo <pablo.a.moreno@gmail.com>
2013-10-08 10:51:00 +0100

    Adds the compound to bioassay writer and updates other writers.

--- src/main/java/uk/ac/cam/cimr/autophagy/exec/RunPubChemAutophagyRet.java
+++ src/main/java/uk/ac/cam/cimr/autophagy/exec/RunPubChemAutophagyRet.java
@@ -4,6 +4,7 @@ import uk.ac.cam.cimr.autophagy.criteria.PubChemCountActiveMolInAssay;
 import uk.ac.cam.cimr.autophagy.criteria.PubChemIC50uMolBelow;
 import uk.ac.cam.cimr.autophagy.io.BAssayBagAssaySummaryWriter;
 import uk.ac.cam.cimr.autophagy.io.BioAssayBagWriter;
+import uk.ac.cam.cimr.autophagy.io.Compound2BioAssayWriter;
 import uk.ac.cam.cimr.autophagy.io.HighlyActiveCompoundsBioAssayBagWriter;
 import uk.ac.cam.cimr.autophagy.ws.BioAssayAnnotator;
 import uk.ac.cam.cimr.autophagy.ws.BioAssayBag;
@@ -33,7 +34,7 @@ public class RunPubChemAutophagyRet {
 
     public void run() {
         ScreenRetrieval ret = new ScreenRetrieval();
-        ret.setTestingLimit(100);
+        ret.setTestingLimit(30);
         BioAssayBag bag = ret.getAssaysForQuery(query);
         bag.addCriteria(new PubChemCountActiveMolInAssay(), new PubChemIC50uMolBelow(20.0f));
         BioAssayAnnotator annotator = new BioAssayAnnotator();
@@ -49,8 +50,11 @@ public class RunPubChemAutophagyRet {
         String path = "/tmp/";
         BioAssayBagWriter highlyActiveCompoundsBioAssayBagWriter = new HighlyActiveCompoundsBioAssayBagWriter(path);
         BioAssayBagWriter bioAssaySummaryWriter = new BAssayBagAssaySummaryWriter(path);
+        BioAssayBagWriter comp2AssayWriter = new Compound2BioAssayWriter(path);
 
-        RunPubChemAutophagyRet runner = new RunPubChemAutophagyRet("autophagy",highlyActiveCompoundsBioAssayBagWriter, bioAssaySummaryWriter);
+        RunPubChemAutophagyRet runner = new RunPubChemAutophagyRet("autophagy",highlyActiveCompoundsBioAssayBagWriter,
+                bioAssaySummaryWriter,
+                comp2AssayWriter);
 
         runner.run();
     }
--- src/main/java/uk/ac/cam/cimr/autophagy/ws/BioAssayAnnotator.java
+++ src/main/java/uk/ac/cam/cimr/autophagy/ws/BioAssayAnnotator.java
@@ -2,8 +2,10 @@ package uk.ac.cam.cimr.autophagy.ws;
 
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
+import uk.ac.ebi.mdk.domain.identifier.PubChemCompoundIdentifier;
 import uk.ac.ebi.metabolomes.webservices.EUtilsWebServiceConnection;
 import uk.ac.ebi.metabolomes.webservices.eutils.PubChemBioAssayESummaryResult;
+import uk.ac.ebi.metabolomes.webservices.eutils.PubChemNamesResult;
 import uk.ac.ebi.metabolomes.webservices.pubchem.PChemBioAssayTable;
 
 import javax.ws.rs.core.MultivaluedMap;
@@ -22,38 +24,45 @@ import java.util.Map;
 public class BioAssayAnnotator {
 
 
+    private EUtilsWebServiceConnection connection;
+
+    public BioAssayAnnotator() {
+        this.connection = new EUtilsWebServiceConnection();
+    }
+
     /**
      * Annotates bio assays elements in BioAssayBag, currently with name, description, and sources.
+     *
      * @param bag
      */
     public void annotate(BioAssayBag bag) {
         List<PChemBioAssayTable> assays = bag.getAssays();
-            for (int from = 0; from < assays.size(); from+=5000) {
-                int to = from + 200 - 1 > assays.size() ? assays.size() - 1 : from + 200 - 1;
+        for (int from = 0; from < assays.size(); from+=5000) {
+            int to = from + 200 - 1 > assays.size() ? assays.size() - 1 : from + 200 - 1;
 
-                List<String> aids = new ArrayList<String>(to - from + 1);
+            List<String> aids = new ArrayList<String>(to - from + 1);
 
-                for (int i = from ; i <= to ; i++)
-                    aids.add(assays.get(i).getID());
+            for (int i = from ; i <= to ; i++)
+                aids.add(assays.get(i).getID());
 
-                Map<String, PubChemBioAssayESummaryResult> resultsMapped = getPubChemBioAssayESummaryResultMap(aids);
+            Map<String, PubChemBioAssayESummaryResult> resultsMapped = getPubChemBioAssayESummaryResultMap(aids);
 
-                for (int i = from; i <= to; i++) {
-                    PChemBioAssayTable assay = assays.get(i);
-                    PubChemBioAssayESummaryResult result = resultsMapped.get(assay.getID());
-                    assay.setDescription(result.getDescription());
-                    assay.setName(result.getName());
-                    assay.setSources(result.getSources());
-                }
-
+            for (int i = from; i <= to; i++) {
+                PChemBioAssayTable assay = assays.get(i);
+                PubChemBioAssayESummaryResult result = resultsMapped.get(assay.getID());
+                assay.setDescription(result.getDescription());
+                assay.setName(result.getName());
+                assay.setSources(result.getSources());
             }
+        }
+
+        PubChemNamesResult names = connection.getNamesForPubChemCompoundIdentifiers(bag.getOrderedCIDs());
+        bag.setCompoundNames(names);
 
     }
 
     private Map<String, PubChemBioAssayESummaryResult> getPubChemBioAssayESummaryResultMap(List<String> aids) {
-        EUtilsWebServiceConnection connection = new EUtilsWebServiceConnection();
         List<PubChemBioAssayESummaryResult> results = connection.getPubChemBioassaysSummaries(aids);
-
         Map<String,PubChemBioAssayESummaryResult> resultsMapped = new HashMap<String, PubChemBioAssayESummaryResult>(results.size());
         for (PubChemBioAssayESummaryResult result : results) {
             resultsMapped.put(result.getId(),result);

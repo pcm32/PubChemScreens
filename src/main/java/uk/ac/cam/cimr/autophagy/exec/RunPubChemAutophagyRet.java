4f5597e962734af4d9226c969251e915ab140d41
Pablo <pablo.a.moreno@gmail.com>
2013-10-09 09:31:35 +0100

    Adds testing option and logging to the main executable.

--- src/main/java/uk/ac/cam/cimr/autophagy/exec/RunPubChemAutophagyRet.java
+++ src/main/java/uk/ac/cam/cimr/autophagy/exec/RunPubChemAutophagyRet.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.cimr.autophagy.exec;
 
+import org.apache.log4j.Logger;
 import uk.ac.cam.cimr.autophagy.criteria.PubChemCountActiveMolInAssay;
 import uk.ac.cam.cimr.autophagy.criteria.PubChemIC50uMolBelow;
 import uk.ac.cam.cimr.autophagy.io.BAssayBagAssaySummaryWriter;
@@ -11,6 +12,8 @@ import uk.ac.cam.cimr.autophagy.ws.BioAssayBag;
 import uk.ac.cam.cimr.autophagy.ws.ScreenRetrieval;
 
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +27,10 @@ import java.util.List;
  */
 public class RunPubChemAutophagyRet {
 
+    private static final Logger LOGGER = Logger.getLogger(RunPubChemAutophagyRet.class);
+
     String query;
+    Integer testingLimit = 0;
     List<BioAssayBagWriter> writers;
 
     public RunPubChemAutophagyRet(String query, BioAssayBagWriter... writers) {
@@ -32,22 +38,33 @@ public class RunPubChemAutophagyRet {
         this.writers = Arrays.asList(writers);
     }
 
+    public void setTestingLimit(Integer limit) {
+        testingLimit = limit;
+    }
+
     public void run() {
         ScreenRetrieval ret = new ScreenRetrieval();
-        ret.setTestingLimit(30);
+        if(testingLimit>0)
+            ret.setTestingLimit(testingLimit);
         BioAssayBag bag = ret.getAssaysForQuery(query);
+        LOGGER.info("Retrieved bag with "+bag.getAssays().size()+" assays");
         bag.addCriteria(new PubChemCountActiveMolInAssay(), new PubChemIC50uMolBelow(20.0f));
         BioAssayAnnotator annotator = new BioAssayAnnotator();
         annotator.annotate(bag);
+        LOGGER.info("Annotated the bag.");
         bag.compute();
+        LOGGER.info("Computed the bag");
         for (BioAssayBagWriter writer : writers) {
             writer.write(bag);
+            LOGGER.info("Writer "+writer.getClass().toString()+" done.");
         }
+
     }
 
 
     public static void main(String[] args) throws IOException {
-        String path = "/tmp/";
+        String path = args[0];
+        Files.createDirectory(FileSystems.getDefault().getPath(path));
         BioAssayBagWriter highlyActiveCompoundsBioAssayBagWriter = new HighlyActiveCompoundsBioAssayBagWriter(path);
         BioAssayBagWriter bioAssaySummaryWriter = new BAssayBagAssaySummaryWriter(path);
         BioAssayBagWriter comp2AssayWriter = new Compound2BioAssayWriter(path);
@@ -55,7 +72,9 @@ public class RunPubChemAutophagyRet {
         RunPubChemAutophagyRet runner = new RunPubChemAutophagyRet("autophagy",highlyActiveCompoundsBioAssayBagWriter,
                 bioAssaySummaryWriter,
                 comp2AssayWriter);
-
+        if(args.length > 1) {
+            runner.setTestingLimit(Integer.parseInt(args[1]));
+        }
         runner.run();
     }
 
